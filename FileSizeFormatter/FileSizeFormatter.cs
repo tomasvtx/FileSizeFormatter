3aef18334cf327bbe0102badd0eada4202f89dcc
tomasvtx <tomasvytiska@seznam.cz>
2023-12-22 22:15:12 +0100

    Přidejte soubory projektu.

---
+++ FileSizeFormatter/FileSizeFormatter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    public class FileSizeFormatter
+        {
+        private static readonly string[] SizeSuffixes =
+                ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+        private static readonly string[] SizeSuffixesInternet =
+                ["b", "Kb", "Mb", "Gb", "Tb", "Pb", "Eb", "Zb", "Yb"];
+
+        private static string FormatSizeCommon(long size , string [] suffixes)
+            {
+            var index = 0;
+            double formattedSize = size;
+
+            while (formattedSize >= 1024 && index < suffixes.Length - 1)
+                {
+                formattedSize /= 1024;
+                index++;
+                }
+
+            return $"{formattedSize:N1} {suffixes [index]}";
+            }
+
+        public static string FormatSize(long size) => FormatSizeCommon(size , SizeSuffixes);
+
+        public static string FormatSizeInternet(long size) => FormatSizeCommon(size * 8 , SizeSuffixesInternet);
+        }
